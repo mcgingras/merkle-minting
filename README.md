04200e0a5336bad893da78565e309e01c4348b7c
Michael Gingras <mcg79@cornell.edu>
2021-04-13 00:20:28 -0400

    v1

---
+++ README.md
@@ -0,0 +1,2 @@
+# Merkle Minting
+Lazy Minting NFTs using Merkle trees as proof of contents.
---
+++ contracts/MerkleDistributor.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Indentifier: UNLICENSED
+pragma solidity=0.7.0;
+
+import "@openzeppelin/contracts/cryptography/MerkleProof.sol";
+import "./interfaces/IMerkleDistributor.sol";
+
+contract MerkleDistributor is IMerkleDistributor {
+    bytes32 public immutable override merkleRoot;
+
+    mapping (uint256 => uint256) private claimedBitMap;
+
+    constructor(bytes32 _merkleRoot) public {
+        merkleRoot = _merkleRoot;
+    }
+
+    function isClaimed(uint256 index) public view override returns (bool) {
+        uint256 claimedWordIndex = index / 256;
+        uint256 claimedBitIndex = index % 256;
+        uint256 claimedWord = claimedBitMap[claimedWordIndex];
+        uint256 mask = (1 << claimedBitIndex);
+        return claimedWord & mask == mask;
+    }
+
+    function _setClaimed(uint256 index) private {
+        uint256 claimedWordIndex = index / 256;
+        uint256 claimedBitIndex = index % 256;
+        claimedBitMap[claimedWordIndex] = claimedBitMap[claimedWordIndex] | (1 << claimedBitIndex);
+    }
+
+    function claim(uint256 index, address account, uint256 amount, bytes32[] calldata merkleProof) external override {
+        require(!isClaimed(index), 'MerkleDistributor: Drop already claimed.');
+
+        // Verify the merkle proof.
+        bytes32 node = keccak256(abi.encodePacked(index, account, amount));
+        require(MerkleProof.verify(merkleProof, merkleRoot, node), 'MerkleDistributor: Invalid proof.');
+
+        // Mark it claimed and send the token.
+        _setClaimed(index);
+        require(IERC20(token).transfer(account, amount), 'MerkleDistributor: Transfer failed.');
+
+        emit Claimed(index, account, amount);
+    }
+}
